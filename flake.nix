e355fd256fef1bc75bcbde2701457bd37307a3f4
Gregory Mulvaney <gregtmulvaney@gmail.com>
2025-06-03 18:31:35 -0500

    Updates

--- flake.nix
+++ flake.nix
@@ -2,27 +2,39 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    talhelper.url = "github:budimanjojo/talhelper";
   };
 
-  output = {
+  outputs = {
     self,
     nixpkgs,
     flake-utils,
+    talhelper,
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
+        talhelperPkg = talhelper.packages.${system}.default;
       in {
         devShell = pkgs.mkShell {
           name = "k8s";
 
+          shellHook = ''
+            export ROOT_DIR=$(pwd)
+            export SOPS_AGE_KEY_FILE=$ROOT_DIR/age.key
+            export KUBECONFIG=$ROOT_DIR/kubeconfig
+            export TALOSCONFIG=$ROOT_DIR/talos/clusterconfig/talosconfig
+          '';
+
           buildInputs = with pkgs; [
+            bash
             k9s
             fluxcd
             go-task
             talosctl
             kustomize
             kubectl
+            kubeconform
             kubernetes-helm
             sops
             cilium-cli
@@ -34,11 +46,8 @@
             age
             cue
             makejinja
+            talhelperPkg
           ];
-
-          KUBECONFIG = "./kubeconfig";
-          SOPS_AGE_KEY_FILE = "age.key";
-          TALOSCONFIG = "./talos/clusterconfig/talosconfig";
         };
       }
     );
